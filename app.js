d6d822a7c571cd5568d8616501aae1c330da7cd1
Misbah Hasan <tahermisbah41@gmail.com>
2021-02-14 16:44:22 +0600

    again also change

--- app.js
+++ app.js
@@ -16,16 +16,18 @@ const KEY = '15674931-a9d714b6e9d654524df198e00&q';
 // show images 
 const showImages = (images) => {
   imagesArea.style.display = 'block';
+
   gallery.innerHTML = '';
   // show gallery title
   galleryHeader.style.display = 'flex';
   images.forEach(image => {
     let div = document.createElement('div');
     div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
-    div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
-    gallery.appendChild(div)
-  })
+    div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}"> <div><h5>${image.tags}</h5></div>`;
+    gallery.appendChild(div);
+    toggleSpinner();
 
+  });
 }
 
 const getImages = (query) => {
@@ -38,13 +40,18 @@ const getImages = (query) => {
 let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
-  element.classList.toggle('added');
+
+  element.classList.add('added');
 
   let item = sliders.indexOf(img);
+
   if (item === -1) {
     sliders.push(img);
   } else {
-    sliders.push();
+    element.classList.toggle('added');
+    if (item != -1) {
+      delete sliders[item]
+    }
 
   }
 }
@@ -76,6 +83,7 @@ const createSlider = () => {
     src="${slide}"
     alt="">`;
     sliderContainer.appendChild(item)
+
   })
   changeSlide(0)
   timer = setInterval(function () {
@@ -121,20 +129,21 @@ searchBtn.addEventListener('click', function () {
   document.querySelector('.main').style.display = 'none';
   clearInterval(timer);
   const search = document.getElementById('search');
-  getImages(search.value)
+  getImages(search.value);
   sliders.length = 0;
 });
 
 sliderBtn.addEventListener('click', function () {
-  const duration = document.getElementById('duration').value ;
+  const duration = document.getElementById('duration').value;
   if (duration > 0) {
-    createSlider()
+    createSlider();
 
   } else {
-    alert('nagetive Number Is Not Allowed');
+    document.getElementById('duration').value = 1000;
 
   };
 });
+
 // enetr duration
 document.getElementById('duration').addEventListener('keypress', function (e) {
   if (e.key === "Enter") {
@@ -142,3 +151,13 @@ document.getElementById('duration').addEventListener('keypress', function (e) {
   }
 });
 
+
+// spinner
+const toggleSpinner = () => {
+  const spinner = document.getElementById('loadingSpinner');
+  const gallery = document.querySelector('.gallery');
+
+  spinner.classList.toggle('d-none');
+  gallery.classList.toggle('d-none');
+
+};
